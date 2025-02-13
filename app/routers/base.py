8ad33656f39a9217ad0cd06e90297983a02e5a24
Farrid Putra <farridputra@gmail.com>
2025-02-13 13:37:23 +0700

    update routers tags

--- app/routers/base.py
+++ app/routers/base.py
@@ -8,9 +8,9 @@ from . import hero_publisher_route, hero_route, users_route
 def register_all_routers(app: FastAPI):
 
     # Include all routers in here
-    app.include_router(users_route.router, prefix=f"{settings.API_PREFIX}/users", tags=["users"])
-    app.include_router(hero_publisher_route.router, prefix=f"{settings.API_PREFIX}/hero-publishers", tags=["hero_publishers"])
-    app.include_router(hero_route.router, prefix=f"{settings.API_PREFIX}/heroes", tags=["heroes"])
+    app.include_router(users_route.router, prefix=f"{settings.API_PREFIX}/users", tags=["Users"])
+    app.include_router(hero_publisher_route.router, prefix=f"{settings.API_PREFIX}/hero-publishers", tags=["Hero Publishers"])
+    app.include_router(hero_route.router, prefix=f"{settings.API_PREFIX}/heroes", tags=["Heroes"])
     # app.include_router(
     #     admin.router,
     #     prefix="/admin",
