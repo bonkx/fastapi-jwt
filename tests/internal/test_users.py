450a3fe26614191d7921ed53e8a84394df35ff4d
Farrid Putra <farridputra@gmail.com>
2025-02-18 21:49:16 +0700

    test auth_service

--- tests/internal/test_users.py
+++ tests/internal/test_users.py
@@ -9,19 +9,24 @@ from sqlmodel import Field, Session, SQLModel, and_, col, or_, select
 
 from app.core.email import fm
 from app.core.security import create_url_safe_token, decode_url_safe_token
-from app.models import UserCreate, UserProfileCreate
+from app.models import (User, UserCreate, UserLoginModel, UserProfileCreate,
+                        UserUpdate)
 from app.repositories.user_repo import UserRepository
+from app.services.auth_service import AuthService
 
 from . import pytest, pytestmark
+
+fake = Faker()
 
 
 class TestInternalUsers:
     @pytest.fixture(autouse=True)
-    def init(self,  client, api_prefix, db_session, payload_user_register):
+    def init(self,  client, api_prefix, db_session, payload_user_register, payload_user_login):
         self.client = client
         self.api_prefix = api_prefix
         self.db_session = db_session
         self.payload_user_register = payload_user_register
+        self.payload_user_login = payload_user_login
         self.url = f"admin{self.api_prefix}/users/"
 
     async def test_get_user_route(self):
@@ -29,28 +34,130 @@ class TestInternalUsers:
         user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
         assert "id" in user.model_dump()
 
+        # update user role to Admin, verified status
+        user.is_verified = True
+        user.verified_at = datetime.now()
+        user.profile.status_id = 1
+        user.profile.role = "Admin"
+
+        # update user
+        await UserRepository(self.db_session).add_one(user)
+        # await self.db_session.refresh(user)
+
+        assert user.is_verified == True
+        assert user.profile.status_id == 1
+        assert user.profile.role == "Admin"
+
+        # generate token
+        token = await AuthService(self.db_session).login_user(UserLoginModel(**self.payload_user_login))
+
+        access_token = token.access_token
+        headers = {"Authorization": f"Bearer {access_token}"}
+
         # get user
         url = f"{self.url}{user.id}"
-        response = await self.client.get(url)
+        response = await self.client.get(url, headers=headers)
         data = response.json()
-        # print(data)
+        print(data)
 
         assert response.status_code == status.HTTP_200_OK
         assert "id" in data
         assert data["username"] == "johndoe"
 
-    async def test_delete_user(self):
+    async def test_get_user_route_no_token(self):
         # create user
         user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
         assert "id" in user.model_dump()
 
-        # get user
+        # get user without token
+        url = f"{self.url}{user.id}"
+        response = await self.client.get(url)
+        data = response.json()
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert data["detail"] == "Not authenticated"
+
+    # async def test_delete_user(self):
+    #     # create user
+    #     user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
+    #     assert "id" in user.model_dump()
+
+    #     # update user role to Admin, verified status
+    #     user.is_verified = True
+    #     user.verified_at = datetime.now()
+    #     user.profile.status_id = 1
+    #     user.profile.role = "Admin"
+
+    #     # update user
+    #     await UserRepository(self.db_session).add_one(user)
+
+    #     assert user.is_verified == True
+    #     assert user.profile.status_id == 1
+    #     assert user.profile.role == "Admin"
+
+    #     # generate token
+    #     token = await AuthService(self.db_session).login_user(UserLoginModel(**self.payload_user_login))
+
+    #     access_token = token.access_token
+    #     headers = {"Authorization": f"Bearer {access_token}"}
+
+    #     # create user for deletion
+    #     fake_email = fake.email()
+    #     fake_user = await UserRepository(self.db_session).create(UserCreate(
+    #         first_name=fake.first_name(),
+    #         last_name=fake.last_name(),
+    #         username=fake.user_name(),
+    #         email=fake_email,
+    #         password=fake.password(),
+    #     ))
+    #     assert fake_user.email == fake_email
+
+    #     # delete user fake
+    #     url = f"{self.url}{fake_user.id}"
+    #     response = await self.client.delete(url, headers=headers)
+
+    #     assert response.status_code == status.HTTP_204_NO_CONTENT
+    #     # assert response.text == ""
+
+    #     # # Try to get the deleted item
+    #     response = await self.client.get(url, headers=headers)
+    #     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_delete_user_no_token(self):
+        # create user
+        user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
+        assert "id" in user.model_dump()
+
+        # delete user
         url = f"{self.url}{user.id}"
         response = await self.client.delete(url)
+        data = response.json()
 
-        assert response.status_code == status.HTTP_204_NO_CONTENT
-        assert response.text == ""
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert data["detail"] == "Not authenticated"
 
-        # Try to get the deleted item
-        response = await self.client.get(url)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
+    async def test_token_exception(self):
+        # create user
+        user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
+        assert "id" in user.model_dump()
+
+        # update user role to Admin, verified status
+        user.is_verified = True
+        user.verified_at = datetime.now()
+        user.profile.status_id = 1
+
+        # update user
+        await UserRepository(self.db_session).add_one(user)
+
+        assert user.is_verified == True
+
+        headers = {"Authorization": f"Bearer wrong_token"}
+
+        # get user
+        url = f"{self.url}{user.id}"
+        response = await self.client.get(url, headers=headers)
+        data = response.json()
+        print(data)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert data["detail"] == "Token is invalid Or expired"
---
+++ tests/services/test_auth_service.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+
+from app.models import User, UserCreate, UserLoginModel, UserUpdate
+from app.services.auth_service import AuthService
+from app.services.user_service import UserService
+from app.utils.exceptions import AccountNotVerified, InvalidCredentials
+
+from . import pytest, pytestmark
+
+
+async def test_login_account_not_verified(db_session, payload_user_register, payload_user_login):
+    # create user
+    new_user = await UserService(db_session).create(UserCreate(**payload_user_register))
+    assert new_user.email == "johndoe123@fastapi.com"
+
+    # login
+    with pytest.raises(AccountNotVerified) as exc:
+        await AuthService(db_session).login_user(UserLoginModel(**payload_user_login))
+
+    assert exc.type == AccountNotVerified
+
+
+async def test_invalid_credentials(db_session, payload_user_register, payload_user_login):
+    # create user
+    new_user = await UserService(db_session).create(UserCreate(**payload_user_register))
+    assert new_user.email == "johndoe123@fastapi.com"
+
+    # verify user
+    user = await UserService(db_session).verify_user(new_user)
+
+    assert user.is_verified == 1
+    assert user.profile.status_id == 1
+
+    # login
+    # set wrong password
+    payload_user_login["password"] = "wrong password"
+    with pytest.raises(InvalidCredentials) as exc:
+        await AuthService(db_session).login_user(UserLoginModel(**payload_user_login))
+
+    assert exc.type == InvalidCredentials
