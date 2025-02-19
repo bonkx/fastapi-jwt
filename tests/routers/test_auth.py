f30bc8ff4b715191854cd6431bc1aac2fae6a283
Farrid Putra <farridputra@gmail.com>
2025-02-19 12:06:12 +0700

    add setup_user routes.test_auth

--- tests/routers/test_auth.py
+++ tests/routers/test_auth.py
@@ -144,23 +144,27 @@ class TestVerification:
         self.payload_user_register = payload_user_register
         self.url = f"/account/"
 
-    async def test_verify_token(self):
+    @pytest.fixture(autouse=True)
+    async def setup_user(self):
         # register user
         new_user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
         assert "id" in new_user.model_dump()
 
+        self.user = new_user
+
+    async def test_verify_token(self):
         # get user by username
-        get_user = await UserRepository(self.db_session).get_by_username(new_user.username)
-        assert get_user.email == new_user.email
+        get_user = await UserRepository(self.db_session).get_by_username(self.user.username)
+        assert get_user.email == self.user.email
 
         # generate token
         expiration_datetime = datetime.now(UTC) + timedelta(seconds=3600)  # 60 minutes
-        token = await create_url_safe_token({"email": new_user.email, "exp": expiration_datetime})
+        token = await create_url_safe_token({"email": self.user.email, "exp": expiration_datetime})
 
         # decode token to get email
         decode_token = await decode_url_safe_token(token)
         assert "email" in decode_token
-        assert decode_token["email"] == new_user.email
+        assert decode_token["email"] == self.user.email
 
         # request verify link
         url = f"{self.url}verify/{token}"
@@ -174,27 +178,23 @@ class TestVerification:
         assert h1.text == "Account Verification Successful!"
 
         # get user again for checking is_verified and status
-        user = await UserRepository(self.db_session).get_by_id(new_user.id)
+        user = await UserRepository(self.db_session).get_by_id(self.user.id)
         assert user.is_verified == 1
         assert user.profile.status_id == 1
 
     async def test_verify_token_failed(self):
-        # register user
-        new_user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
-        assert "id" in new_user.model_dump()
-
         # get user by email
-        get_user = await UserRepository(self.db_session).get_by_email(new_user.email)
-        assert get_user.email == new_user.email
+        get_user = await UserRepository(self.db_session).get_by_email(self.user.email)
+        assert get_user.email == self.user.email
 
         # generate token
         expiration_datetime = datetime.now(UTC) + timedelta(seconds=3600)  # 60 minutes
-        token = await create_url_safe_token({"email": new_user.email, "exp": expiration_datetime})
+        token = await create_url_safe_token({"email": self.user.email, "exp": expiration_datetime})
 
         # decode token to get email
         decode_token = await decode_url_safe_token(token)
         assert "email" in decode_token
-        assert decode_token["email"] == new_user.email
+        assert decode_token["email"] == self.user.email
 
         # request verify link with failed token value
         url = f"{self.url}verify/{token}_token_failed"
@@ -208,23 +208,19 @@ class TestVerification:
         assert h1.text == "Oops... Invalid Token"
 
         # get user again for checking is_verified and status
-        user = await UserRepository(self.db_session).get_by_id(new_user.id)
+        user = await UserRepository(self.db_session).get_by_id(self.user.id)
         assert user.is_verified == 0
         assert user.profile.status_id == 3
 
     async def test_user_already_verified(self):
-        # register user
-        new_user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
-        assert "id" in new_user.model_dump()
-
         # generate token
         expiration_datetime = datetime.now(UTC) + timedelta(seconds=3600)  # 60 minutes
-        token = await create_url_safe_token({"email": new_user.email, "exp": expiration_datetime})
+        token = await create_url_safe_token({"email": self.user.email, "exp": expiration_datetime})
 
         # decode token to get email
         decode_token = await decode_url_safe_token(token)
         assert "email" in decode_token
-        assert decode_token["email"] == new_user.email
+        assert decode_token["email"] == self.user.email
 
         # request verify link
         url = f"{self.url}verify/{token}"
@@ -246,16 +242,12 @@ class TestVerification:
         assert h1.text == "Account already verified"
 
     async def test_resend_verification_link(self):
-        # register user
-        new_user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
-        assert "id" in new_user.model_dump()
-
-        token_resend = await create_url_safe_token({"email": new_user.email, "action": "resend_verification_link"})
+        token_resend = await create_url_safe_token({"email": self.user.email, "action": "resend_verification_link"})
 
         # decode token to get email
         decode_token = await decode_url_safe_token(token_resend)
         assert "email" in decode_token
-        assert decode_token["email"] == new_user.email
+        assert decode_token["email"] == self.user.email
 
         fm.config.SUPPRESS_SEND = 1
         with fm.record_messages() as outbox:
@@ -271,16 +263,12 @@ class TestVerification:
             assert outbox[0]['To'] == "johndoe123@fastapi.com"
 
     async def test_resend_verification_link_failed(self):
-        # register user
-        new_user = await UserRepository(self.db_session).create(UserCreate(**self.payload_user_register))
-        assert "id" in new_user.model_dump()
-
-        token_resend = await create_url_safe_token({"email": new_user.email, "action": "resend_verification_link"})
+        token_resend = await create_url_safe_token({"email": self.user.email, "action": "resend_verification_link"})
 
         # decode token to get email
         decode_token = await decode_url_safe_token(token_resend)
         assert "email" in decode_token
-        assert decode_token["email"] == new_user.email
+        assert decode_token["email"] == self.user.email
 
         fm.config.SUPPRESS_SEND = 1
         with fm.record_messages() as outbox:
